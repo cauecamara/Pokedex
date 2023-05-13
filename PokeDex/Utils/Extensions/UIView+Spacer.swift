71dfc06163251a1f9e817dc5c24f6c2d4fe90862
CaueCamara <cauecamara@gmail.com>
2023-05-13 15:11:58 -0700

    add Spacer

---
+++ PokeDex/Utils/Extensions/UIView+Spacer.swift
@@ -0,0 +1,24 @@
+//
+//  UIView+Spacer.swift
+//  PokeDex
+//
+//  Created by Caue Camara on 13/05/23.
+//
+
+import UIKit
+
+extension UIView {
+
+    static func spacer(size: CGFloat = 10, for layout: NSLayoutConstraint.Axis = .horizontal) -> UIView {
+        let spacer = UIView()
+        
+        if layout == .horizontal {
+            spacer.widthAnchor.constraint(equalToConstant: size).isActive = true
+        } else {
+            spacer.heightAnchor.constraint(equalToConstant: size).isActive = true
+        }
+        
+        return spacer
+    }
+
+}
